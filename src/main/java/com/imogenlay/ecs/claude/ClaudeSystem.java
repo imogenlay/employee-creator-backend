9a25dc7241cba5d22c4c95f9b3caf244f75f7310
imogenlay <imogen@imogenlay.com>
2026-03-16 09:39:08 +1100

    Fixed testing failure due to no api key in test application.properties

--- src/main/java/com/imogenlay/ecs/claude/ClaudeSystem.java
+++ src/main/java/com/imogenlay/ecs/claude/ClaudeSystem.java
@@ -5,12 +5,24 @@ public class ClaudeSystem
 	public static final String MODEL = "claude-haiku-4-5-20251001"; // Sonnet: claude-sonnet-4-20250514
 	public static final int MAX_TOKENS = 1024;
 	public static final String STOP_REASON_TOOL_USE = "tool_use";
-	public static final String TOOL_GET_ALL_EMPLOYEES = "get_all_employees";
 	public final static String URL = "https://api.anthropic.com/v1/messages";
 	public final static String PROMPT = """ 
 			You are an assistant that is helping a user navigate an Employee Creator application.
 			Your goal is to help the user access specific employee information with queries.
-			Responses will be printed to a chat box with very limited formatting.
-			All text will be displayed as is, but a highlighted inline-block can be shown if
+						
+			You have access to a few tools that can access information about employees and other information.
+			Tools can be used sequentially. Use multiple tools if needed.
+									
+			If questions are ambiguous in regards to subject matter, assume that the question relates to database information.
+			Provide clear and comprehensive answers to all questions asked.
+			Don't make assumptions about the data. No, somebody living near a lake does not mean they want to become a marine biologist.
+			Attempt to keep a conversation going but offering further possibilities of tasks you could perform.
+			Unless specifically asked for, you do not need to give an employee's ID when reading out their name.
+						
+			Responses are printed to a chat box as is. Do not attempt to use markdown formatting.
+			The only exception is that a highlighted inline-block can be shown if
 			surrounded with a single ` character on either side. The block is monospace and has an outline.""";
+
+
+	public static final String TOOL_GET_ALL_EMPLOYEES = "get_all_employees";
 }
--- src/main/java/com/imogenlay/ecs/config/ApplicationConfig.java
+++ src/main/java/com/imogenlay/ecs/config/ApplicationConfig.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ApplicationConfig
 {
 	@Bean
